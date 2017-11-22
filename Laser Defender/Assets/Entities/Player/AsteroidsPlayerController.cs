f187fb1a23cfcfb55b953ab3c98f76d20f39eb65
swert7662 <swert7662@gmail.com>
2017-11-22 03:58:25 -0800

    Initial Asteroids Mode Push
    -Edited Player Controller to have alternate controls for Asteroids mode:
    
    --Ship rotates to mouse pos
    --Projectiles now launch from empty children that hold spawn positions
    --Thrusters rotate with ship correctly
    --Mouse click to fire
    --Created interesting parallaxing effect by adding space bg particle
    effects to ship to make it look like it traverses space
    -Also added asteroids assets and sprites

---
+++ Laser Defender/Assets/Entities/Player/AsteroidsPlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidsPlayerController : MonoBehaviour {
+
+    //Public Vars
+    public Camera camera;
+    public float speed;
+
+    //Private Vars
+    private Vector3 mousePosition;
+    private Vector3 direction;
+    private float distanceFromObject;
+
+    void FixedUpdate() {
+        //Grab the current mouse position on the screen
+        mousePosition = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z - camera.transform.position.z));
+
+        //Rotates toward the mouse
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2((mousePosition.y - transform.position.y), (mousePosition.x - transform.position.x)) * Mathf.Rad2Deg - 90);
+
+    }//End Fire3 If case
+}
+     
--- Laser Defender/Assets/Entities/Player/PlayerController.cs
+++ Laser Defender/Assets/Entities/Player/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    public Camera camera;
     public float health = 250f;
     public float speed = 15.0f;
     public float padding = 1f;
@@ -11,16 +12,20 @@ public class PlayerController : MonoBehaviour {
     public float fireRate = 0.2f;
     public AudioClip laserSound;
     public AudioClip deathSound;
+    public bool AsteroidsMode = false;
 
     bool coroutineCalled = false;
     bool colorChange = false;
     bool damaged = false;
-
+    
     float xMin;
     float xMax;
     float altFire = 1;
-	// Use this for initialization
-	void Start () {
+    Transform beamSpawnPos;
+    private Vector3 mousePosition;
+
+    // Use this for initialization
+    void Start () {
         float distance = transform.position.z - Camera.main.transform.position.z;
         Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
         Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
@@ -30,7 +35,8 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        playerInput();
+        if (!AsteroidsMode) { playerInput(); }
+        else if (AsteroidsMode) { playerInputAsteroidsMode(); }        
 	}
 
     void playerInput() {
@@ -50,13 +56,35 @@ public class PlayerController : MonoBehaviour {
         float newX = Mathf.Clamp(transform.position.x, xMin, xMax);
         transform.position = new Vector3(newX, transform.position.y, transform.position.z);
     }
+    void playerInputAsteroidsMode() {
+        //Grab the current mouse position on the screen
+        mousePosition = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z - camera.transform.position.z));
+
+        //Rotates toward the mouse
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2((mousePosition.y - transform.position.y), (mousePosition.x - transform.position.x)) * Mathf.Rad2Deg - 90);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            InvokeRepeating("Fire", 0.000001f, fireRate);
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0)) {
+            CancelInvoke("Fire");
+        }
+    }
 
     void Fire() {
         altFire *= -1;
-        Vector3 beamPos = new Vector3( transform.position.x + (altFire * .28f), (transform.position.y + .6f), 0);
+        if (altFire < 0) { beamSpawnPos = this.gameObject.transform.GetChild(2); }
+        else beamSpawnPos = this.gameObject.transform.GetChild(3);
+        Vector3 beamPos = new Vector3(beamSpawnPos.position.x, beamSpawnPos.position.y, 0);
+        print(beamPos);
         AudioSource.PlayClipAtPoint(laserSound, transform.position);
-        GameObject beam = Instantiate(projectile, beamPos, Quaternion.identity) as GameObject;
-        beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, projectileSpeed, 0);
+        GameObject beam = Instantiate(projectile, beamPos, transform.rotation) as GameObject;
+        if (!AsteroidsMode) { beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, projectileSpeed, 0); }
+        else if (AsteroidsMode) {
+            Vector3 direction = mousePosition - transform.position;
+            beam.GetComponent<Rigidbody2D>().velocity = direction * projectileSpeed;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
